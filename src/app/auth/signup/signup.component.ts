e3407761381de620124e044886ef10bb3e7713d5
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2024-01-02 12:50:02 +0100

    feat(signup.component.html): add form error display to improve user feedback feat(signup.component.ts): import MatchPassword validator to ensure password and confirmation match feat(match-password.spec.ts): add test for MatchPassword validator to ensure functionality feat(match-password.ts): create MatchPassword validator to enforce password confirmation match

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
-
+import { MatchPassword } from "../validators/match-password";
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -24,9 +24,9 @@ export class SignupComponent {
       Validators.minLength(4),
       Validators.maxLength(20),
       ])
-  });
+  }, { validators: [this.matchPassword.validate] });
 
-  constructor() { }
+  constructor(private matchPassword: MatchPassword) { }
 
   ngOnInit(): void {
   }
