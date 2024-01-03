8a1446fa7eb8b7be6f995595ce36164bb65b30de
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2024-01-03 18:19:21 +0100

    feat(app-routing.module.ts): add authGuard to 'inbox' route to protect it from unauthorized access feat(auth.guard.ts): create new authGuard to check if user is authenticated before route activation test(auth.guard.spec.ts): add unit tests for authGuard to ensure it works as expected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InboxModule } from './inbox/inbox.module';
+import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: 'inbox', loadChildren: () => InboxModule },
+  { path: 'inbox', canLoad: [authGuard], loadChildren: () => InboxModule },
 ];
 
 @NgModule({
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanMatchFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { take, skipWhile, tap } from 'rxjs';
+import { inject } from '@angular/core';
+
+export const authGuard: CanMatchFn = (route, segments) => {
+  const router = inject(Router); // Get the Router instance using Angular's inject function
+  const authService = inject(AuthService); // Similarly, get the AuthService instance
+
+  return authService.signedin$.pipe(
+    skipWhile(value => value === null), // Skip while the authentication status is unknown (null)
+    take(1), // Take the first emitted value that is not null
+    tap(authenticated => {
+      if (!authenticated) {
+        router.navigate(['/']); // Redirect to the home page if not authenticated
+        return false; // Prevent route activation
+      }
+      return true; // Allow route activation
+    })
+  );
+};
