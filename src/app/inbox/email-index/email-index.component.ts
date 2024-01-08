b136b91ca00122a5a7803456efbacc7c27515811
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2024-01-08 17:11:18 +0100

    feat(email-index): add email listing functionality in email-index component feat(email.service): add getEmails method to fetch emails from server feat(home): integrate email-index component into home component feat(inbox-routing): add routing for email-show and placeholder components feat(placeholder): add new placeholder component for not-found routes style(email-index, placeholder): add basic styling for email-index and placeholder components refactor(inbox.module): import and declare new placeholder component

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { EmailService } from '../email.service';
 
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
   styleUrls: ['./email-index.component.css']
 })
-export class EmailIndexComponent {
 
+export class EmailIndexComponent {
+  emails: EmailSummary[] = []; // Explicitly type the emails variable as an array of EmailSummary objects
+  constructor(private emailService: EmailService) {
+
+  }
+
+  ngOnInit() {
+    this.emailService.getEmails().subscribe((emails: EmailSummary[]) => { // Explicitly type the emails parameter as an array of EmailSummary objects
+      this.emails = emails;
+    })
+  }
 }
--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 @Injectable({
   providedIn: 'root'
 })
-export class EmailService {
 
-  constructor() { }
+
+export class EmailService {
+  rootUrl = 'https://api.angular-email.com/emails';
+
+
+  constructor(private htttp: HttpClient) { }
+
+  getEmails() {
+    return this.htttp.get<EmailSummary[]>(this.rootUrl);
+  }
 }
