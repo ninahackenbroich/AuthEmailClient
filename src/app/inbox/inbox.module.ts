6d2f7e8ffcd5ad45805b0f28545536eaf5b64d94
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2024-01-09 18:11:54 +0100

    feat(email-index): add CSS classes for content and trim to improve layout and readability feat(email-index): apply trim class to email subject and from fields to prevent overflow feat(email-resolver): add error handling to redirect to not-found page when email is not found feat(email-show): add divider and email content display to email-show component feat(home): add modal component to home component for compose functionality feat(inbox-routing): replace PlaceholderComponent with NotFoundComponent for not-found route feat(inbox): add NotFoundComponent to InboxModule and import SharedModule feat(not-found): create NotFoundComponent to handle non-existent emails feat(modal): create ModalComponent for generic modal functionality feat(shared): add ModalComponent to SharedModule for reuse across the app

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -8,6 +8,8 @@ import { EmailReplyComponent } from './email-reply/email-reply.component';
 import { EmailIndexComponent } from './email-index/email-index.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
@@ -17,11 +19,13 @@ import { PlaceholderComponent } from './placeholder/placeholder.component';
     EmailReplyComponent,
     EmailIndexComponent,
     EmailShowComponent,
-    PlaceholderComponent
+    PlaceholderComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    SharedModule
   ]
 })
 export class InboxModule { }
