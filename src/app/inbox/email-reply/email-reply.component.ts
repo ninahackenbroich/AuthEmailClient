09525b4bb908383ec04aac2aee389ba3d3e513e0
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2024-01-10 15:47:07 +0100

    feat(email-reply): add reply functionality to email component - Add a reply button to email-reply.component.html - Import necessary dependencies in email-reply.component.ts - Implement reply functionality in email-reply.component.ts - Add email-reply component to email-show.component.html
    This feature allows users to reply to emails directly from the email view.

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class EmailReplyComponent {
 
+  showModal = false;
+  @Input() email!: Email;
+
+  constructor(private authService: AuthService, private emailService: EmailService) {
+
+  }
+
+  ngOnInit() {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+    this.email = {
+      ...this.email,
+      id: '',
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      html: this.email.html,
+      text: `\n\n\n------------- ${this.email.from} wrote: \n\n> ${text}`,
+      from: this.email.to,
+    };
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
